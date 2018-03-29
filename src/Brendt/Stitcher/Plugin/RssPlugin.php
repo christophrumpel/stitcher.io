a2a466109bcfded663c12eeaff3a0f038d0c71d2
Brent Roose <brent@spatie.be>
2018-03-29 15:23:38 +0200

    RSS support

--- src/Brendt/Stitcher/Plugin/RssPlugin.php
+++ src/Brendt/Stitcher/Plugin/RssPlugin.php
@@ -3,24 +3,47 @@
 namespace Brendt\Stitcher\Plugin;
 
 use Brendt\Stitcher\App;
-use Brendt\Stitcher\Factory\AdapterFactory;
+use Brendt\Stitcher\Event\Event;
+use Brendt\Stitcher\Parser\Site\SiteParser;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Filesystem\Filesystem;
 
-class RssPlugin implements Plugin {
+class RssPlugin implements Plugin, EventSubscriberInterface {
 
     public function __construct() {
-        /** @var AdapterFactory $adapterFactory */
-        $adapterFactory = App::get('factory.adapter');
+        /** @var \Symfony\Component\EventDispatcher\EventDispatcher $eventDispatcher */
+        $eventDispatcher = App::get('service.event.dispatcher');
 
-        $adapterFactory->addAdapter('rss', function () {
-            return App::get('adapter.rss');
-        });
+        $eventDispatcher->addSubscriber($this);
+    }
+
+    public function onPageParsed(Event $event)
+    {
+        $pageId = $event->getData()['pageId'] ?? null;
+
+        if ($pageId !== 'rss') {
+            return;
+        }
+
+        $publicDirectory = App::getParameter('directories.public');
+
+        $fs = new Filesystem();
+
+        $fs->copy($publicDirectory . '/rss.html', $publicDirectory . '/rss.xml');
     }
 
     public static function getConfigPath() {
-        return;
+        return null;
     }
 
     public static function getServicesPath() {
-        return __DIR__ . '/../../../../config/services.rss.yml';
+        return null;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            SiteParser::EVENT_PAGE_PARSED => 'onPageParsed',
+        ];
     }
 }
